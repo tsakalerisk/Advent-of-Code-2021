af631f0ac6bb2bc785fa5d7a1fbb7a14894ee51a
Konstantinos Tsakaleris <13333629+tsakalerisk@users.noreply.github.com>
2022-04-27 21:01:14 +0300

    Day 3 Clean-Up

--- Day3/src/main/kotlin/Main.kt
+++ Day3/src/main/kotlin/Main.kt
@@ -6,8 +6,9 @@ fun main() {
     val mostCommon = mutableListOf<Int>()
     val leastCommon = mutableListOf<Int>()
     for (i in 0 until lines[0].size) {
-        lines.groupingBy { it[i] }.eachCount().maxByOrNull { it.value }?.let { mostCommon.add(it.key) }
-        lines.groupingBy { it[i] }.eachCount().minByOrNull { it.value }?.let { leastCommon.add(it.key) }
+        val freq = digitFrequencies(lines, i)
+        freq.maxByOrNull { it.value }?.let { mostCommon.add(it.key) }
+        freq.minByOrNull { it.value }?.let { leastCommon.add(it.key) }
     }
     val gamma = binListToInt(mostCommon)
     val epsilon = binListToInt(leastCommon)
@@ -16,7 +17,7 @@ fun main() {
     println("Power consumption (Part 1 answer): " + gamma * epsilon)
 
     //Part 2
-    val comparator = { x: Map.Entry<Int,Int>, y: Map.Entry<Int,Int> ->
+    val comparator = { x: Map.Entry<Int, Int>, y: Map.Entry<Int, Int> ->
         when {
             (x.value > y.value) -> 1
             (x.value == y.value && x.key == 1) -> 1
@@ -25,26 +26,22 @@ fun main() {
     }
     val oxygenCandidates = lines.toMutableList()
     for (i in 0 until oxygenCandidates[0].size) {
-        val mostCommonDigit = oxygenCandidates.groupingBy { it[i] }.eachCount().maxWithOrNull(comparator)?.key
+        val mostCommonDigit = digitFrequencies(oxygenCandidates, i).maxWithOrNull(comparator)?.key
         oxygenCandidates.removeIf { it[i] != mostCommonDigit }
-        if (oxygenCandidates.isEmpty()) break
     }
     val co2Candidates = lines.toMutableList()
     for (i in 0 until co2Candidates[0].size) {
-        val leastCommonDigit = co2Candidates.groupingBy { it[i] }.eachCount().minWithOrNull(comparator)?.key
+        val leastCommonDigit = digitFrequencies(co2Candidates, i).minWithOrNull(comparator)?.key
         co2Candidates.removeIf { it[i] != leastCommonDigit }
-        if (co2Candidates.isEmpty()) break
     }
 
-    if (oxygenCandidates.isNotEmpty() && co2Candidates.isNotEmpty()) {
-        val oxygenRating = binListToInt(oxygenCandidates[0])
-        val co2Rating = binListToInt(co2Candidates[0])
-        println("Oxygen generator rating: $oxygenRating")
-        println("CO2 scrubber rating: $co2Rating")
-        println("Life support rating (Part 2 Answer): " + oxygenRating * co2Rating)
-    }
-    else
-        println("One of the lists is empty!")
+    val oxygenRating = binListToInt(oxygenCandidates[0])
+    val co2Rating = binListToInt(co2Candidates[0])
+    println("Oxygen generator rating: $oxygenRating")
+    println("CO2 scrubber rating: $co2Rating")
+    println("Life support rating (Part 2 Answer): " + oxygenRating * co2Rating)
 }
 
-fun binListToInt(list: List<Int>):Int = Integer.parseInt(list.joinToString(separator = ""), 2)
+fun binListToInt(list: List<Int>): Int = Integer.parseInt(list.joinToString(separator = ""), 2)
+
+fun digitFrequencies(list: List<List<Int>>, digit: Int): Map<Int, Int> = list.groupingBy { it[digit] }.eachCount()
